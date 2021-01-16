bf83b4cb3788d32dfb6e332d0d8560309fa62684
Marcelo-Diament <marcelodiament@gmail.com>
2021-01-16 16:36:58 -0300

    feature: profile route

--- routes.js
+++ routes.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => res.render('index'))
+router.get('/profile', (req, res) => res.render('profile'))
 
 module.exports = router
