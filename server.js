2a034ec4df90513c7fb62f6717a7bf57e7e463a1
Marcelo-Diament <marcelodiament@gmail.com>
2021-01-16 10:02:19 -0300

    feature: import routes.js

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const path = require('path')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const router = require('./routes')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', router)
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
