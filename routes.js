bbcb94f7613b5daa14f3c6570f831c9d6b365c8d
Marcelo-Diament <marcelodiament@gmail.com>
2021-01-17 01:27:20 -0300

    feature: profile route

--- routes.js
+++ routes.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const upload = require('./middleware/upload')
+const Resize = require('./Resize')
+const path = require('path')
 
 router.get('/', (req, res) => res.render('index'))
-router.get('/profile', (req, res) => res.render('profile'))
+router.post('/profile', upload.single('image'), async function (req, res) {
+
+  const username = req.body.name
+  const surname = req.body.lastname
+  const imagePath = path.join(__dirname, '/public/images')
+  const fileUpload = new Resize(imagePath)
+
+  if (!req.file) {
+    res.status(401).json({
+      error: 'Please provide an image'
+    })
+  }
+
+  const filename = await fileUpload.save(req.file.buffer)
+  return res.status(200).render('profile', {
+    imagename: filename,
+    firstname: username,
+    lastname: surname
+  })
+})
 
 module.exports = router
