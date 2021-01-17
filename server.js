e7fcb62df36be2371a21fab598fe0f02823b30a8
Marcelo-Diament <marcelodiament@gmail.com>
2021-01-17 00:44:28 -0300

    feature: static files path definition

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/', router)
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
